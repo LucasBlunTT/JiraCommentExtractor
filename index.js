fa88b3189165d9a8776e300df6d44c8020f96fb3
Lucas da Silva <sklucassilva@gmail.com>
2024-08-19 17:36:02 -0300

    feat: Adicionar dependência do Puppeteer e extrair comentários de página da web

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import puppeteer from 'puppeteer';
 import fs from 'fs';
 import path from 'path';
 
+// Função para fazer login na página
 async function login(page, username, password) {
   await page.goto(
-    'https://portalcliente.pixeon.com/secure/QueuesExtensionAction.jspa?queue=1555938793940',
+    'https://portalcliente.pixeon.com/issues/?filter=73577&jql=project%20%3D%20SSD%20AND%20issuetype%20in%20(Incidente%2C%20D%C3%BAvida%2C%20Implanta%C3%A7%C3%B5es%2C%20Task%2C%20InfraService)%20AND%20cf%5B31739%5D%20in%20(%22PACS%20Aurora%20%5B36447%5D%22%2C%20%22PixPrint%20%5B36448%5D%22%2C%20%22Docscan%20%5B36441%5D%22%2C%20%22Capta%20%5B36437%5D%22%2C%20%22DICOM%20Router%20%5B36440%5D%22%2C%20%22ClickVita%20%5B36439%5D%22%2C%20%22X-Clinic%20%5B36454%5D%22%2C%20%22Korus%20%5B39277%5D%22%2C%20%22Pleres%20Desktop%20%5B36449%5D%22%2C%20%22Pleres%20Net%20%5B36450%5D%22%2C%20%22SMARTCLIN%20%5B36451%5D%22%2C%20%22SMARTHEALTH%20%5B36453%5D%22%2C%20%22SMARTLAB%20%5B36452%5D%22%2C%20%22LABLINK%20%5B36444%5D%22%2C%20%22Cetus%20%5B36438%5D%22%2C%20%22LABLINK%2FCOMMUNIS%20%5B36446%5D%22%2C%20%22LABLINK%20PRO%20%5B36445%5D%22%2C%20%22Aurora%20Drive%20%5B48243%5D%22)%20AND%20filter%20not%20in%20(%22Clientes%20Governo%22%2C%20%22Clientes%20GNDI%22)%20AND%20cf%5B31739%5D%20in%20(%22PACS%20Aurora%20%5B36447%5D%22%2C%20%22PixPrint%20%5B36448%5D%22%2C%20%22Docscan%20%5B36441%5D%22%2C%20%22Capta%20%5B36437%5D%22%2C%20%22DICOM%20Router%20%5B36440%5D%22%2C%20%22ClickVita%20%5B36439%5D%22%2C%20%22Aurora%20Drive%20%5B48243%5D%22)%20AND%20(issueFunction%20in%20commented(%22by%20leonardo.fernandes%20after%20startOfMonth(-1)%22)%20OR%20status%20changed%20by%20leonardo.fernandes%20after%20startOfMonth(-1))',
   );
   await page.setViewport({ width: 1080, height: 1024 });
 
@@ -12,9 +13,45 @@ async function login(page, username, password) {
   await page.type('#login-form-password', password);
   await page.click('#login-form-submit');
 
-  await page.waitForSelector('.aui');
+  await page.waitForSelector('#issuetable');
 }
 
+// Função para extrair comentários dos links
+async function extractCommentsFromHref(page, href) {
+  await page.goto(href);
+  await page.waitForSelector('.toggle-title');
+
+  const comentarioLeonardo = await page.evaluate(() => {
+    const autorComentario = document.querySelector(
+      'a[rel="leonardo.fernandes"]',
+    );
+    if (autorComentario) {
+      const comentarioDiv = autorComentario
+        .closest('.activity-comment')
+        .querySelector('.action-body.flooded');
+      return comentarioDiv ? comentarioDiv.textContent : null;
+    }
+    return null;
+  });
+
+  if (comentarioLeonardo) {
+    const comentarioCliente = await page.evaluate(() => {
+      const comentario = document.querySelector(
+        '.user-content-block p',
+      )?.textContent;
+      return comentario;
+    });
+
+    return {
+      comentarioLeonardo,
+      comentarioCliente: comentarioCliente || null,
+    };
+  }
+
+  return null;
+}
+
+// Função principal para extrair todos os links dos chamados e seus comentários
 async function extractComments(page) {
   await page.waitForSelector('tbody tr .issuetype a');
 
@@ -35,46 +72,15 @@ async function extractComments(page) {
   let allComments = '';
 
   for (let href of hrefs) {
-    await page.goto(href);
-    await page.waitForSelector('.toggle-title');
-
-    // Extraindo o comentário do Leonardo Fernandes como texto puro
-    const comentarioLeonardo = await page.evaluate(() => {
-      const autorComentario = document.querySelector(
-        'a[rel="leonardo.fernandes"]',
-      );
-      if (autorComentario) {
-        const comentarioDiv = autorComentario
-          .closest('.activity-comment')
-          .querySelector('.action-body.flooded');
-        return comentarioDiv ? comentarioDiv.textContent : null;
-      }
-      return null;
-    });
-
-    if (comentarioLeonardo) {
-      allComments += `COMENTÁRIO DO LEONARDO FERNANDES: {${comentarioLeonardo}}\n\n\n\n + ========================= + \n\n\n\n`;
-
-      // Extraindo o comentário do cliente se o do Leonardo for encontrado
-      const comentarioCliente = await page.evaluate(() => {
-        const comentario = document.querySelector(
-          '.user-content-block p',
-        )?.textContent;
-        return comentario;
-      });
-
-      if (comentarioCliente) {
-        allComments += `COMENTÁRIO DO CLIENTE: {${comentarioCliente}}\n\n\n\n`;
-      } else {
-        allComments += 'COMENTÁRIO DO CLIENTE NÃO ENCONTRADO.\n\n';
-      }
+    const comments = await extractCommentsFromHref(page, href);
+    if (comments) {
+      allComments += `COMENTÁRIO DO LEONARDO FERNANDES: {${comments.comentarioLeonardo}}\n\n\n\n + ========================= + \n\n\n\n`;
+      allComments += `COMENTÁRIO DO CLIENTE: {${comments.comentarioCliente}}\n\n\n\n`;
     }
   }
 
-  // Caminho absoluto para o arquivo chamados.txt
   const filePath = path.resolve('chamados.txt');
 
-  // Escrevendo todos os comentários no arquivo chamados.txt
   try {
     fs.writeFileSync(filePath, allComments);
     console.log('Comentários extraídos e salvos em chamados.txt');
